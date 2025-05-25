fb7d82bc176d6e089ea9c1f8453e44a0e3730711
Michael Rasch <raschmichael10@gmail.com>
2025-05-24 20:36:21 -0400

    Added cors support

--- src/main/java/com/michael/App.java
+++ src/main/java/com/michael/App.java
@@ -1,9 +1,12 @@
 package com.michael;
 
+import com.michael.filter.CorsFilter;
 import com.michael.servlet.router.RouterServlet;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.FilterDef;
+import org.apache.tomcat.util.descriptor.web.FilterMap;
 
 import java.util.Set;
 
@@ -11,15 +14,30 @@ public class App {
     public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-
         tomcat.setBaseDir("temp");
-
         Context ctx = tomcat.addContext("", System.getProperty("java.io.tmpdir"));
 
-        RouterServlet routerServlet = new RouterServlet(Set.of());
-        Tomcat.addServlet(ctx, "routerServlet", routerServlet);
+        addFilters(ctx);
+        addServlets(ctx);
 
         tomcat.start();
         tomcat.getServer().await();
+    }
+
+    private static void addServlets(Context ctx) {
+        RouterServlet routerServlet = new RouterServlet(Set.of());
+        Tomcat.addServlet(ctx, "routerServlet", routerServlet);
+    }
+
+    private static void addFilters(Context ctx) {
+        FilterDef filterDef = new FilterDef();
+        filterDef.setFilterName("cors");
+        filterDef.setFilterClass(CorsFilter.class.getName());
+        ctx.addFilterDef(filterDef);
+
+        FilterMap filterMap = new FilterMap();
+        filterMap.setFilterName("cors");
+        filterMap.addURLPattern("/*");
+        ctx.addFilterMap(filterMap);
     }
 }
---
+++ src/main/java/com/michael/filter/CorsFilter.java
@@ -0,0 +1,31 @@
+package com.michael.filter;
+
+import com.michael.http.HttpMethod;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        response.setHeader("access-control-allow-origin", "*");
+
+        HttpMethod method = HttpMethod.valueOf(request.getMethod());
+
+        if (method == HttpMethod.OPTION) {
+            response.setHeader("access-control-allow-methods", "*");
+            response.setHeader("access-control-allow-headers", "*");
+            response.setHeader("access-control-max-age", "86400");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
